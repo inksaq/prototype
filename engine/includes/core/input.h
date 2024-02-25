394f68dac313710e58e273681d798b928531f1dc
inksaq <inksaq@protonmail.com>
2024-02-25 21:39:25 +1300

    duplicate

---
+++ engine/includes/core/input.h
@@ -0,0 +1,24 @@
+
+    #include "GLFW/glfw3.h"
+namespace input {
+        extern double scrollX, scrollY;
+        void scrollCallback(GLFWwindow* window, double xoffset, double yoffset);
+    }
+
+    class InputHandler {
+        private:
+            GLFWwindow* window;
+            double &scrollX, &scrollY;
+
+        public:
+            InputHandler(GLFWwindow* window);
+            ~InputHandler();
+
+            bool getKey(int32_t key);
+            bool getMouseButton(int32_t button);
+            void getCursorPosition(double* x, double* y);
+            void setCursorPosition(double x, double y);
+            void setCursorVisible(bool visible);
+            void destroy();
+    };
+
